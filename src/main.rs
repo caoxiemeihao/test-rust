bbfaf1e1357766a35d42b5457c24bddf31fcc352
草鞋没号 <308487730@qq.com>
2021-10-15 18:17:22 +0800

    test:

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ mod playground;
 fn main() {
     // playground::hello_warp::serve();
     // playground::fs::read();
-    playground::json::parse();
+    // playground::json::parse();
+    // playground::data_operate::parse();
+    playground::guessing_name::start();
 }
--- src/playground/mod.rs
+++ src/playground/mod.rs
@@ -1,4 +1,6 @@
 
-pub mod hello_warp;
-pub mod fs;
-pub mod json;
+// pub mod hello_warp;
+// pub mod fs;
+// pub mod json;
+// pub mod data_operate;
+pub mod guessing_name;
