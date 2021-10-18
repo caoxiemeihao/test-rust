4163b5eeee9b55c577f881347de098c40870b1b8
草鞋没号 <308487730@qq.com>
2021-10-18 10:31:47 +0800

    https://weread.qq.com/web/reader/d733256071eeeed9d7322fdk6f4322302126f4922f45dec

--- src/playground/guessing_name.rs
+++ src/playground/guessing_name.rs
@@ -1,6 +1,29 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
+pub fn start() {
+    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("Please input your guess.");
+    
+    io::stdin().read_line(&mut guess)
+    .expect("Failed to read line");
+    
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+}
+
+/*
 pub fn start() {
     println!("Guess the number!");
 
@@ -17,3 +40,4 @@ pub fn start() {
 
     println!("You guessed: {}", guess);
 }
+*/
