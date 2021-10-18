235ab2fc011c4bbebb002a90a06a198ec40c1523
草鞋没号 <308487730@qq.com>
2021-10-18 10:11:50 +0800

    test: rand

--- src/playground/guessing_name.rs
+++ src/playground/guessing_name.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 pub fn start() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
